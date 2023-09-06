60d3d7c999edab09e9249844078a0bdd0576665c
Zdthns <Zdthns25@gmail.com>
2023-09-06 15:10:29 +0300

    1

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,4 +1,5 @@
-require("dotenv").config({path: "./.env.deploy"});
+const dotenv = require("dotenv");
+dotenv.config({path: './.env.deploy'});
 
 const {DEPLOY_USER, DEPLOY_HOST, DEPLOY_REF, DEPLOY_REPOSITORY, DEPLOY_PATH} = process.env;
 
@@ -15,7 +16,7 @@ module.exports = {
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
       'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
-      'post-deploy': 'cd backend && pwd && npm ci && npm run build $$ pm2 startOrRestart ecosystem.config.js --env production',
+      'post-deploy': 'cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
 }
