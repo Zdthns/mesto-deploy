51441c943928195901d0f9f4a7e03d0ee8a651fb
Zdthns <Zdthns25@gmail.com>
2023-09-07 08:16:25 +0300

    123

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,5 +1,5 @@
 const dotenv = require("dotenv");
-dotenv.config({path: './.env'});
+dotenv.config({path: './.env.deploy'});
 
 const {DEPLOY_USER, DEPLOY_HOST, DEPLOY_REF, DEPLOY_REPOSITORY, DEPLOY_PATH} =  process.env;
 
@@ -15,8 +15,10 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
-      'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
-      'post-deploy': 'cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env prjduction',
+     'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'npm i && npm run build',
+      // 'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
+      //  'post-deploy': 'cd backend && pwd && npm i && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env prjduction',
     },
   },
 }
