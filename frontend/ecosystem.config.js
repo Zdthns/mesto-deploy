4ba1a53abad3c8b0c382b9b3141ff550c3259f29
Anna-Kr <Zdthns25@gmail.com>
2023-09-08 17:58:08 +0300

    123

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,18 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env.deploy" });
+
+const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_REF, DEPLOY_REPOSITORY, DEPLOY_PATH } =
+  process.env;
+
+module.exports = {
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPOSITORY,
+      path: DEPLOY_PATH,
+      "post-deploy": "cd frontend && pwd && npm i && npm run build",
+    },
+  },
+};
