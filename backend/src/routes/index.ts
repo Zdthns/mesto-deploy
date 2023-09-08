25700fc532dd2c025bda62d84cefeba95fd70c03
Анна <89003491+Zdthns@users.noreply.github.com>
2023-09-08 12:15:59 +0300

    Revert "Develop"

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,4 +1,4 @@
-import express, {
+import {
   Router, Request, Response, NextFunction,
 } from 'express';
 import userRouter from './users';
@@ -9,13 +9,8 @@ import {
   createUser, login,
 } from '../controllers/users';
 import { validateUserBody, validateAuthentication } from '../middlewares/validatons';
-const app = express();
+
 const router = Router();
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-}); 
 router.post('/signup', validateUserBody, createUser);
 router.post('/signin', validateAuthentication, login);
 
