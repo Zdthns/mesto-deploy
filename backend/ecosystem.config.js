e0249dfb65d645b3803a08efbd82f79a8924f509
Zdthns <Zdthns25@gmail.com>
2023-09-11 11:21:15 +0300

    extra files

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -17,9 +17,7 @@ module.exports = {
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
       repo: DEPLOY_REPOSITORY,
-      path: DEPLOY_PATH,
-      //  'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      //   'post-deploy': 'npm i && npm run build',
+      path: DEPLOY_PATH,  
       "pre-deploy-local": `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
       "post-deploy":
         "cd backend && pwd && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production",
